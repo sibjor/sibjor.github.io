a6c2bbbe7076460a6fcfcd4db73e000774c01b91
sibjor <sixteen610@gmail.com>
2025-05-05 21:34:53 +0200

    Smaller refactoring in global.css

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,19 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const observer = new IntersectionObserver((entries) => { 
-        entries.forEach((entry) => {
-            console.log(entry.target);
-        })
-    }); 
-    
-    observer.observe(document.querySelector('#About-you'));
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const aboutYouHeader = document.querySelector("#about-you");
+
+    const observer = new IntersectionObserver(
+        (entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    aboutYouHeader.classList.add("animate");
+                    observer.unobserve(aboutYouHeader); // Stop observing after animation
+                }
+            });
+        },
+        { threshold: 1.0 } // Trigger when 50% of the element is visible
+    );
+
+    observer.observe(aboutYouHeader);
 });
